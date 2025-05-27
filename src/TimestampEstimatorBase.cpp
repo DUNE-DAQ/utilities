4aba3947a70f31142ec750cb0b2dade7fa332a5d
Kurt Biery <biery@daq.fnal.gov>
2025-05-27 13:23:17 -0500

    Fixed a couple of bugs in TimestampEstimator and TimestampEstimatorBase. The former was causing the RandomTriggerCandidateMaker to produce an incorrect interval between the first two triggers in a run. Tests were added to TimestampEstimator_test.cxx to demonstrate the problems (before the fixes were made) and validate the changes.

--- src/TimestampEstimatorBase.cpp
+++ src/TimestampEstimatorBase.cpp
@@ -12,6 +12,17 @@
 
 namespace dunedaq {
 namespace utilities {
+
+/**
+ * @brief Waits for a valid timestamp to become available.  Returns a status value that
+ *        indicates whether a valid timestamp is available or not.
+ * @param continue_flag whether to continue waiting until a valid timestamp is available
+ *        or return immediately
+ * @details The value of the continue_flag can be changed from true to false externally
+ *          to this method, and that will cause the method to exit soon thereafter
+ *          (on the order of 10 msec) and return the current status.
+ * @return kFinished if a valid timestamp is available or kInterrupted if one is not
+ */
 TimestampEstimatorBase::WaitStatus
 TimestampEstimatorBase::wait_for_valid_timestamp(std::atomic<bool>& continue_flag)
 {
@@ -23,7 +34,9 @@ TimestampEstimatorBase::wait_for_valid_timestamp(std::atomic<bool>& continue_fla
     }
   }
 
-  return continue_flag.load() ? TimestampEstimatorBase::kFinished : TimestampEstimatorBase::kInterrupted;
+  // 27-May-2025, KAB: modified this return statement so that the return code is based on whether a
+  // valid timestamp is available (instead of whether the caller asked the method to wait or not)
+  return (get_timestamp_estimate() != std::numeric_limits<uint64_t>::max()) ? TimestampEstimatorBase::kFinished : TimestampEstimatorBase::kInterrupted;
 }
 
 TimestampEstimatorBase::WaitStatus
