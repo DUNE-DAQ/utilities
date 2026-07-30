d2e81d2218c9ef4b9d30aac4fe3ea7de62c34ec2
Kurt Biery <biery@daq.fnal.gov>
2026-07-30 12:08:05 -0500

    Switched to using the TTEST TRACE macro in DelayManager.hpp to check whether artificial delays are globally enabled.

--- include/utilities/DelayManager.hpp
+++ include/utilities/DelayManager.hpp
@@ -48,20 +48,21 @@ public:
   inline bool maybe_delay(std::string const& delay_name,
                           std::string const& detail_msg) ///< Returns true if delay occured
   {
-    bool delays_are_globally_enabled = false;
-    TLOG_DEBUG(42) << "Delays are enabled globally is " << (delays_are_globally_enabled = true);
+    const int GLOBAL_ENABLE_TRACE_LEVEL = 42;
 
     if (m_delay_map.contains(delay_name)) {
       uint32_t delay_usec = m_delay_map[delay_name];
       if (delay_usec > 0) {
-        if (delays_are_globally_enabled) {
+        // check if the user has set the appropriate TRACE level, which we use as a global enable
+        if (TTEST(GLOBAL_ENABLE_TRACE_LEVEL)) {
           ers::warning(ArtificialDelay(ERS_HERE, delay_usec, delay_name, detail_msg));
           usleep(delay_usec);
           return true;
         } else {
           TLOG_DEBUG(15) << "NOTE: a delay of " << delay_usec << " usec was *not* executed "
                          << "for delay_name " << delay_name << " because delays are not "
-                         << "globally enabled. Consider 'tonM -n DelayManager.hpp DEBUG+42'.";
+                         << "globally enabled. Consider 'tonM -n DelayManager.hpp DEBUG+"
+                         << GLOBAL_ENABLE_TRACE_LEVEL << "'.";
         }
       }
     }
